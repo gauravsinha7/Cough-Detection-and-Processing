d8b57dcbad03afe7d00ab1d8e9be0a2c690a5f38
Gaurav <gauravsinha952@gmail.com>
2020-08-21 23:43:59 +0530

    live cough prediction module using pyaudio

---
+++ live_cough_prediction.py
@@ -0,0 +1,30 @@
+from keras.models import load_model
+import tensorflow as tf
+import numpy as np
+from vggish_input import waveform_to_examples
+import pyaudio
+from pathlib import Path
+import time
+import argparse
+import wget
+
+# Variables
+FORMAT = pyaudio.paInt16
+CHANNELS = 1
+RATE = 16000
+CHUNK = RATE
+MICROPHONES_DESCRIPTION = []
+FPS = 60.0
+
+###########################
+# Check Microphone
+###########################
+print("=====")
+print("1 / 2: Checking Microphones... ")
+print("=====")
+
+import microphones
+desc, mics, indices = microphones.list_microphones()
+if (len(mics) == 0):
+    print("Error: No microphone found.")
+    exit()
